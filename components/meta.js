1c0d721599dea079ac7848bfd41f6f9cec4b95ad
Pradyun Narkadamilli <pradyungn@gmail.com>
2021-01-27 23:49:06 -0800

    Updated date everywhere

--- components/meta.js
+++ components/meta.js
@@ -10,6 +10,7 @@ export default ({
     <title>{title}</title>
     <link rel="shortcut icon" href="/static/logo.png"/>
     <meta property="og:title" content={title} />
+    <meta property="og:image" content={image} />
     <meta name="twitter:title" content={title} />
     <meta name="og:url" content={url} />
     <meta property="og:type" content="website" />
@@ -17,7 +18,6 @@ export default ({
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
     <meta name="twitter:description" content={description} />
-    <meta property="og:image" content={image} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image" content={image} />
     <script
